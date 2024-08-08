697d5462a238cdcee1d87882910f0fc2adf6626c
Manan Computer <sami606713@gmail.com>
2024-08-07 18:57:22 -0700

    adding code in app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from src.components.data_loader import load_data
+from streamlit_option_menu import option_menu
+from src.components.data_processor import process_data
+from src.components.data_visulization import visulaize_data
+import streamlit as st
+import pandas as pd
+
+# Set the page layout
+def layout():
+        selected = option_menu(
+                menu_title=None,
+                options=["Getting Data", 'Processing Data','Visulaize Data','Feature Engnering'], 
+                icons=['📥', '⚙️', '📊', '🛠️'], 
+                menu_icon="Data Cleaning", 
+                default_index=0,
+                orientation="horizontal"
+                )
+        return selected
+
+if __name__=="__main__":
+   
+    # Set the page configration setting
+    st.set_page_config(
+        page_title="Data Science App",
+        page_icon="🧊",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+
+    
+    option=layout()
+    if option == "Getting Data":
+        load_data()
+
+    elif option == "Processing Data":
+        # st.write("Processing Data")
+        df=st.session_state['data']
+        # st.dataframe(df)
+        process_data(df=df)
+
+    elif option == "Visulaize Data":
+        df=st.session_state['data']
+        visulaize_data(df=df)
+    
+    elif option == 'Feature Engnering':
+        st.header("Feature Engnering")
