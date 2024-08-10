97e8f11db8662e2fac01105c484cdad16e26f870
Manan Computer <sami606713@gmail.com>
2024-08-10 11:24:01 -0700

    Adding Kaggle data feature

--- src/components/data_loader.py
+++ src/components/data_loader.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from src.utils import load_kaggle_data
 import os
 
 
@@ -8,16 +9,48 @@ def load_data():
     """
     This fun is responsible for loadind the data
     """
-    st.header("Data Loading")
-    data_path = os.path.join("Data/raw","raw.csv")
-    data_file=st.file_uploader("Upload a file",type=['csv'])
-
+    # set the session state for data
     if "data" not in st.session_state:
         st.session_state['data']=None
 
-    if data_file is not None:
-        # Read the CSV file into a DataFrame
-        df = pd.read_csv(data_file)
+    st.header("Data Loading")
+
+    # set the data path where we can save the data
+    data_path = os.path.join("Data/raw","raw.csv")
+
+    # Now we can add mulitple feature so that user can load data from different sources
+    data_source=st.selectbox("Select the data source",["csv","excel","json","API","Kaggle"])
+
+    if data_source=="csv":
+        data_file=st.file_uploader("Upload a file",type=['csv'])
+        if data_file is not None:
+            df=pd.read_csv(data_file)
+            st.session_state['data']=df
+    
+    elif data_source=="excel":
+        data_file=st.file_uploader("Upload a file",type=['xlsx'])
+        if data_file is not None:
+            df=pd.read_excel(data_file)
+            st.session_state['data']=df
+    
+    elif data_source=="json":
+        data_file=st.file_uploader("Upload a file",type=['json'])
+        if data_file is not None:
+            df=pd.read_json(data_file)
+            st.session_state['data']=df
+    
+    elif data_source=="API":
+        st.write("API is not implemented yet")
+        data_file=None
+
+    elif data_source=="Kaggle":
+        st.write("Kaggle is not implemented yet")
+        df=load_kaggle_data()
+        st.session_state['data']=df
+
+    try:
+        # Read the data from session state
+        df = st.session_state['data']
         
 
         # Add the multiselector so that user can select the specific columns as they want
@@ -37,4 +70,7 @@ def load_data():
         # Save the DataFrame to a file
         df.to_csv(data_path,index=False)
         # Save the data in session state
-        st.session_state['data']=df
+        # st.session_state['data']=df
+    except Exception as e:
+        st.write("Data is not loaded yet")
+        st.write("Please load the data first")
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import os 
 import logging
 import streamlit as st
+import zipfile
+import subprocess
+import shutil
+import pandas as pd
+# import kaggle as kg
 
 
 def save_file(df,path):
@@ -12,3 +17,59 @@ def save_file(df,path):
         logging.info("Data Save successfully")
     except Exception as e:
         return e
+
+# Data loading from kaggle
+def load_kaggle_data():
+        # Get the user datset command
+        kaggle_data_cmd=st.text_input("Enter the Kaggle data api command: ", placeholder="e.g., kaggle datasets download -d username/dataset-name")
+        if kaggle_data_cmd:
+            try:
+                with st.spinner('Wait for download...'):
+                    subprocess.run(kaggle_data_cmd.split(" "),check=True)
+                    # Extract the dataset (assuming it's a zip file)
+                    zip_file = kaggle_data_cmd.split('/')[-1] + ".zip"
+
+                    # Unzip the file
+                    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                        zip_ref.extractall('Data/raw')
+                    
+                    # remove the zip file
+                    os.remove(zip_file)
+                    
+                    # Now extract the file the is downloaded so the we can rename the file
+                    data_files=os.listdir('Data/raw')
+                    rename_files=[]
+                    for target_file in data_files:
+                        if len(target_file)!=7:
+                            # st.write(target_file)
+                            rename_files.append(target_file)
+
+                    if len(rename_files)==1:
+                        if rename_files[0].endswith(".json"):
+                            original_file=os.path.join('Data/raw',rename_files[0])
+                            target_file=os.path.join('Data/raw','raw.json')
+                            shutil.move(original_file,target_file)
+                            df=pd.read_json(target_file)
+                            return df
+                        elif rename_files[0].endswith(".xlsx"):
+                            original_file=os.path.join('Data/raw',rename_files[0])
+                            target_file=os.path.join('Data/raw','raw.xlsx')
+                            shutil.move(original_file,target_file)
+                            df=pd.read_excel(target_file)
+                            return df
+                        else:
+                            original_file=os.path.join('Data/raw',rename_files[0])
+                            target_file=os.path.join('Data/raw','raw.csv')
+                            shutil.move(original_file,target_file)
+                            df=pd.read_csv(target_file)
+                            return df
+    
+                    else:
+                        st.write("Data contains multiple files select one file and remove the other files")
+                        # file=st.multiselect("Select the file",rename_files)
+                        # if file in rename_files:
+                        #     if st.button("Remove File:"):
+                        #         shutil.rmtree(file)
+
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
