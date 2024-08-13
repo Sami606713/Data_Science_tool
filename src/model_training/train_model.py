a57b3507e54157f571cd466dd7c40dbf18180990
Manan Computer <sami606713@gmail.com>
2024-08-13 05:32:55 -0700

    Model Training Scipt Done

--- src/model_training/train_model.py
+++ src/model_training/train_model.py
@@ -1,9 +1,18 @@
 # --------------------------------------Import Packaeges-------------------------------------------#
 import os
+import numpy as np
 import streamlit as st
-from src.utils import explain_error
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from src.utils import (explain_error,numerical_pipeline,
+                       categorical_pipeline,build_transformer,
+                       build_model,model_train,classification_results,regression_results)
 #--------------------------------------------------------------------------------------------------#
 
+#-------------------------------------------Script Session Variable---------------------------------------#
+
+
+# ----------------------------------------------------------------------------------------------------# 
 
 # -------------------------------------------Model Trainer------------------------------------------# 
 def model_trainer(df):
@@ -15,6 +24,7 @@ def model_trainer(df):
         st.subheader("Saperate input and target col",divider="rainbow")
         target_col=st.selectbox("Select Target Col: ",df.columns.tolist(),key="target col")
         feature_df=df.drop(columns=[target_col])
+        label=df[target_col]
 
         if target_col is not None:
             col1,col2=st.columns(2)
@@ -25,9 +35,30 @@ def model_trainer(df):
             
             with col2:
                 with st.container(border=True):
-                    st.write('Target Col')
-                    st.dataframe(df[target_col].head(),use_container_width=True)
+                    st.write('Target Col')   
+                    st.dataframe(label.head(),use_container_width=True)
         #--------------------------------------------------------------------------------------------------#
+
+        #================================Train Test Split================================================#
+        st.subheader("Train Test Split",divider="rainbow")
+        split_ratio=st.number_input("Split Ration: ",min_value=0.1,max_value=0.9,value=0.2)
+        try:
+            with st.status("Splitting Data..."):
+                # Perform the train-test split
+                x_train, x_test, y_train, y_test = train_test_split(
+                    feature_df, label, test_size=split_ratio, random_state=43
+                )
+
+                # Display the shapes of the data
+                st.success(f"Total Shape of data: {feature_df.shape}")
+                st.success(f"Train Data Shape: {x_train.shape}")
+                st.success(f"Test Data Shape: {x_test.shape}")
+        
+        
+        except Exception as e:
+            st.write(f"{e}")
+
+        #=================================================================================================#
 
         #----------------------------Saperate num and cat col----------------------------------#
         st.subheader("Saperate Num and Cat Columns For Dependent Features",divider="rainbow")
@@ -45,11 +76,122 @@ def model_trainer(df):
         with col2:
             with st.container(border=True):
                 cat_col=feature_df.select_dtypes('object').columns
-                if cat_col is not None:
+                if len(cat_col)>0:
                     st.dataframe(feature_df[cat_col].head(),use_container_width=True)
                 else:
-                    st.write("No Categorical Found")
+                    with st.container(border=True):
+                        st.write("No Categorical Col")
         
+        #--------------------------------------------------------------------------------------------------#
+
+
+        #================================Building Pipelines================================================#
+        st.subheader("Building Pipelines",divider="rainbow")
+        col1,col2=st.columns(2)
+        with col1:
+            if len(num_col)>0:
+                with st.container(border=True):
+
+                    steps=numerical_pipeline()
+
+                    num_pipe=Pipeline(steps=steps)
+
+            else:
+                with st.container(border=True):
+                    st.write("No Numerical Col")
+
+        with col2:
+            if len(cat_col)>0:
+                with st.container(border=True):
+                    steps=categorical_pipeline()
+                    cat_pipe=Pipeline(steps=steps)
+
+            else:
+                with st.container(border=True):
+                    st.write("No Categorical Col")
+                    cat_pipe=None
+
+        if st.button("View Pipeline: ",key="Pipelines"):
+            st.subheader("Numerical Pipeline")
+            st.write(num_pipe)
+            st.write("*"*100)
+
+            st.subheader("Categorical Pipeline")
+            st.write(cat_pipe)
+            st.write("*"*100)
+
+
+        #================================Building Transformer================================================#
+        st.subheader("Building Transformer",divider="rainbow")
+        transformer=build_transformer(num_col=num_col,num_pipe=num_pipe,
+                                      cat_col=cat_col,cat_pipe=cat_pipe)
+        if st.button("View Transformers"):
+            st.write(transformer)
+        #--------------------------------------------------------------------------------------------------#
+
+        #================================Model Pipeline================================================#
+        st.subheader("Select Task",divider="rainbow")
+        task_type=st.selectbox('Select Task',["Classification","Regression"])
+        # -------------------------------Classification----------------------------------------------#
+        if task_type=="Classification":
+            st.subheader("Build Model Pipeline",divider="rainbow")
+
+            model=st.selectbox("Select the model",["LogisticRegression","RandomForestClassifier"])
+
+            final_pipeline=build_model(transformer=transformer,model_name=model)
+            if st.button("View Model_Pipeline"):
+                st.write(final_pipeline)
+            
+            st.header("Train Model",divider="rainbow")
+            col1,col2=st.columns(2)
+            with  col1:
+                with st.container(border=True):
+                    st.write("Model Training")
+                    if st.button("Train"):
+                        # with st.status("Model Training..."):
+                        model_train(model_pipe=final_pipeline,x_train=x_train,
+                                    y_train=y_train,x_test=x_test,y_test=y_test)
+                        # st.write(y_pred)
+            
+            with  col2:
+                with st.container(border=True):
+                    st.write("Model Report")
+                    if st.button("Model Report"):
+                        classification_results(actual=st.session_state['y_test_transform']
+                                               ,predicion=st.session_state['prediction'])
+                    
+        # --------------------------------------------------------------------------------------------#
+
+        # -------------------------------Regression----------------------------------------------#
+        if task_type=="Regression":
+            st.subheader("Build Model Pipeline",divider="rainbow")
+            model=st.selectbox("Select the model",["LinearRegression","RandomForestRegressor"])
+            final_pipeline=build_model(transformer=transformer,model_name=model)
+            if st.button("View Model_Pipeline"):
+                st.write(final_pipeline)
+
+            st.header("Train Model",divider="rainbow")
+            col1,col2=st.columns(2)
+            with  col1:
+                with st.container(border=True):
+                    st.write("Model Training")
+                    if st.button("Train"):
+                        # with st.status("Model Training..."):
+                        model_train(model_pipe=final_pipeline,x_train=x_train,
+                                    y_train=y_train,x_test=x_test,y_test=y_test)
+                        # st.write(y_pred)
+            
+            with  col2:
+                with st.container(border=True):
+                    st.write("Model Report")
+                    if st.button("Model Report"):
+                        regression_results(actual=y_test
+                                            ,predicion=st.session_state['prediction'])
+
+        #--------------------------------------------------------------------------------------------------#
+
+        #================================Train Model=======================================================#
+
         #--------------------------------------------------------------------------------------------------#
 
     except Exception as e:
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,15 @@
 # --------------------------------------Import Packaeges-------------------------------------------#
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,StandardScaler,MinMaxScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import (LogisticRegression,LinearRegression)
+from sklearn.ensemble import (RandomForestClassifier,RandomForestRegressor)
+from sklearn.tree import (DecisionTreeClassifier,DecisionTreeRegressor)
+from sklearn.metrics import (accuracy_score,precision_score,recall_score,f1_score,confusion_matrix,
+                             r2_score,mean_absolute_error,mean_squared_error)
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
 import os 
 import logging
 import streamlit as st
@@ -6,12 +17,18 @@ import zipfile
 import subprocess
 import shutil
 import pandas as pd
-from huggingface_hub import InferenceClient
-from dotenv import load_dotenv
 load_dotenv()
+hf_token=os.getenv("hf_token")
 #--------------------------------------------------------------------------------------------------#
 
-hf_token=os.getenv("hf_token")
+#------------------------------------------Script Session variable----------------------------------#
+if "y_test_transform" not in st.session_state:
+    st.session_state['y_test_transform']=None
+
+if "prediction" not in st.session_state:
+    st.session_state['prediction']=None
+# ---------------------------------------------------------------------------------------------------#
+
 # -------------------------------------------Save File---------------------------------------------#
 def save_file(df,path):
     """ 
@@ -130,3 +147,193 @@ def explain_error(error):
     if st.button("Explain"):
         response=chat_response(text=error)
         st.write_stream(response)
+
+
+#==================================Numerical Pipeline============================================#
+def numerical_pipeline():
+    try:
+        st.write('Numerical Pipeline')
+        steps=[]
+        transformation=st.multiselect("apply transformation",
+                                    options=["ImputeMissingValues","Standard Scaler","MinMaxScaler"],
+                                    )
+        if "Standard Scaler" in transformation:
+            steps.append(("StandardScaling",StandardScaler()))
+        
+        if "MinMaxScaler" in transformation:
+            steps.append(("MinMaxScaling",MinMaxScaler()))
+
+        if "ImputeMissingValues" in transformation:
+             stretegy=st.selectbox("select the strategy: ",['mean',"median"])
+             steps.append(("Imputation",SimpleImputer(strategy=stretegy)))
+        
+
+        return steps
+    
+    except Exception as e:
+        st.write(f"{e}")
+#========================================================================================================#
+
+
+#========================================Categorical Pipeline============================================#
+def categorical_pipeline():
+    try:
+        st.write('Categorical Pipeline')
+        steps=[]
+        transformation=st.multiselect("apply transformation",
+                                    options=["ImputeMissingValues","OneHotEncoder","OrdinalEncoder"],
+                                    )
+        if "OneHotEncoder" in transformation:
+            steps.append(("OHE",OneHotEncoder(drop='first',handle_unknown='ignore',sparse_output=False)))
+        
+        if "OrdinalEncoder" in transformation:
+            # st.write("Ordinal Encoder")
+            steps.append(("OEN",OneHotEncoder()))
+
+        if "ImputeMissingValues" in transformation:
+            stretegy=st.selectbox("select the strategy: ",['Most Frequent',"Constant"])
+            if stretegy=='Most Frequent':
+                steps.append(("Imputition",SimpleImputer(strategy="most_frequent",fill_value = None)))
+            if stretegy=="Constant":
+                value=st.text_input("Enter Constant Value: ")
+                steps.append(("constant_imputition",SimpleImputer(strategy = "constant",fill_value = value)))
+
+
+        return steps
+    
+    except Exception as e:
+        st.write(f"{e}")
+
+#========================================================================================================#
+
+
+#========================================Building Transformer============================================#
+def build_transformer(num_col,num_pipe,cat_col,cat_pipe):
+    try:
+        if cat_pipe!=None:
+            transformer=ColumnTransformer(transformers=[
+                ("num_transform",num_pipe,num_col),
+                ("cat_transform",cat_pipe,cat_col)
+            ],remainder='passthrough')
+            return transformer
+        else:
+            transformer=ColumnTransformer(transformers=[
+                ("num_transform",num_pipe,num_col)
+            ],remainder='passthrough')
+            return transformer
+
+    except Exception as e:
+        st.write(f"{e}")
+#========================================================================================================#
+
+#========================================Building Model============================================#
+def get_model(model_name):
+    """
+    This fun can take the model name and return the model
+    args: model_name: str: model name
+    return: model: model object
+    """
+    model_dic={
+        "LogisticRegression":LogisticRegression(),
+        "LinearRegression":LinearRegression(),
+        "RandomForestClassifier":RandomForestClassifier(),
+        "RandomForestRegressor":RandomForestRegressor()
+    }
+    return model_dic[model_name]
+
+
+
+def build_model(transformer,model_name):
+    try:
+        # Get the model 
+        model=get_model(model_name)
+        final=Pipeline(steps=[
+            ("Processor",transformer),
+            ("Model",model)
+        ])
+        return final
+    except Exception as e:
+        st.write(f"{e}")
+#========================================================================================================#
+
+#================================================Train Model=============================================#
+
+def model_train(model_pipe,x_train,x_test,y_train,y_test):
+    if y_train.dtype=="object":
+        try:
+            # Encode the target column
+            encoder=LabelEncoder()
+            y_train_transform=encoder.fit_transform(y_train)
+            y_test_transform=encoder.transform(y_test)
+
+            # Train the model
+            model_pipe.fit(x_train,y_train_transform)
+            st.success("Model Trained Successfully")
+
+            # Prediction
+            st.write("Test Prediction")
+            y_pred=model_pipe.predict(x_test)
+
+            # set the actual and prediction value in session state
+            st.session_state['y_test_transform']=y_test_transform
+            st.session_state['prediction']=y_pred
+        except Exception as e:
+            st.write(f"{e}")
+
+    else:
+        try:
+            # Train the model
+            model_pipe.fit(x_train,y_train)
+            st.success("Model Trained Successfully")
+
+            # Prediction
+            st.write("Test Prediction")
+            y_pred=model_pipe.predict(x_test)
+            st.session_state['prediction']=y_pred
+            
+        except Exception as e:
+            st.write(f"{e}")
+
+#========================================================================================================#
+
+# ===========================================Model Results===============================================#
+# ----------Classification Results------------------#
+def classification_results(actual,predicion):
+    st.write("Classification Results")
+    # actaul value
+    st.write("Actual Value")
+    # st.table(actual)
+
+    # predicted value
+    st.write("predicted Value")
+    # st.table(predicion)
+
+    result={
+        "Accuracy":accuracy_score(actual,predicion),
+        "Precesion":precision_score(actual,predicion,average='weighted'),
+        "Recall":recall_score(actual,predicion,average='weighted'),
+        "F1-Score":f1_score(actual,predicion,average='weighted')
+    }
+    st.dataframe(result,use_container_width=True)
+    st.write("Confussion Matrix")
+    st.write(confusion_matrix(actual,predicion))
+    
+# ----------Regression Results------------------#
+def regression_results(actual,predicion):
+    st.write("Results Results")
+
+        # actaul value
+    st.write("Actual Value")
+    # st.table(actual)
+
+    # predicted value
+    st.write("predicted Value")
+    # st.table(predicion)
+
+    result={
+        "Mean Absolute Error":mean_absolute_error(actual,predicion),
+        "Mean Squared Error":mean_squared_error(actual,predicion),
+        "R2-Score":r2_score(actual,predicion)
+    }
+    st.dataframe(result,use_container_width=True)
+#========================================================================================================#
